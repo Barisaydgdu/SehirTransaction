2008dbb49a3642db40ee22f01bf82a47b303cd45
Barış AYDOĞDU <barisaydogdu76@gmail.com>
2023-11-02 23:24:20 +0300

    Proje Tamamlandı

---
+++ SehirTransaction/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace SehirTransaction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIlAd = new System.Windows.Forms.TextBox();
+            this.txtPlaka = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstIlceler = new System.Windows.Forms.ListBox();
+            this.txtIlceAd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İL:";
+            // 
+            // txtIlAd
+            // 
+            this.txtIlAd.Location = new System.Drawing.Point(98, 31);
+            this.txtIlAd.Name = "txtIlAd";
+            this.txtIlAd.Size = new System.Drawing.Size(236, 20);
+            this.txtIlAd.TabIndex = 1;
+            // 
+            // txtPlaka
+            // 
+            this.txtPlaka.Location = new System.Drawing.Point(98, 57);
+            this.txtPlaka.Name = "txtPlaka";
+            this.txtPlaka.Size = new System.Drawing.Size(236, 20);
+            this.txtPlaka.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PLAKA:";
+            // 
+            // lstIlceler
+            // 
+            this.lstIlceler.FormattingEnabled = true;
+            this.lstIlceler.Location = new System.Drawing.Point(57, 94);
+            this.lstIlceler.Name = "lstIlceler";
+            this.lstIlceler.Size = new System.Drawing.Size(115, 225);
+            this.lstIlceler.TabIndex = 4;
+            // 
+            // txtIlceAd
+            // 
+            this.txtIlceAd.Location = new System.Drawing.Point(227, 94);
+            this.txtIlceAd.Name = "txtIlceAd";
+            this.txtIlceAd.Size = new System.Drawing.Size(107, 20);
+            this.txtIlceAd.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "İLÇE AD:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "İLÇE EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "İL VE İLÇELERİNİ KAYDET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(454, 46);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(155, 273);
+            this.treeView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIlceAd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstIlceler);
+            this.Controls.Add(this.txtPlaka);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIlAd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIlAd;
+        private System.Windows.Forms.TextBox txtPlaka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstIlceler;
+        private System.Windows.Forms.TextBox txtIlceAd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ SehirTransaction/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SehirTransaction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstIlceler.Items.Add(txtIlceAd.Text);
+            txtIlceAd.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SehirlerEntities1 vt = new SehirlerEntities1();
+            using (var transaction = vt.Database.BeginTransaction())
+            {
+                try
+                {
+                    Il yeniIl = new Il();
+                    yeniIl.IlAd = txtIlAd.Text;
+                    yeniIl.IlPlaka = txtPlaka.Text;
+                    vt.Il.Add(yeniIl);
+                    vt.SaveChanges();
+                    int kaydedilenIlID = yeniIl.IlID; //insert edilen primarykey i döndürür
+                    foreach (var item in lstIlceler.Items)
+                    {
+                        Ilce yeniIlce = new Ilce();
+                        yeniIlce.IlId = kaydedilenIlID;
+                        yeniIlce.IlceAd = item.ToString();
+                        vt.Ilce.Add(yeniIlce);
+                        vt.SaveChanges();
+                    }                    
+                }
+                catch (Exception hata)
+                {
+                    transaction.Rollback(); //hata oluşursa verileri eski haline getir
+                }
+                finally
+                {
+                    transaction.Commit(); //hata yoksa işlemi onayla
+                    IlleriDoldur();
+                    txtIlAd.Clear();
+                    txtPlaka.Clear();
+                    lstIlceler.Items.Clear();
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IlleriDoldur();
+        }
+
+
+
+        void IlleriDoldur()
+        {
+            treeView1.Nodes.Clear();
+            //illeri çek
+            SehirlerEntities1 vt = new SehirlerEntities1();
+            List<Il> ilListesi = vt.Il.ToList();
+            List<Ilce> ilceListesi = vt.Ilce.ToList();
+
+            foreach (Il item in ilListesi)
+            {
+                treeView1.Nodes.Add(item.IlID.ToString(),item.IlAd);
+                foreach (var ilce in item.Ilce)
+                {
+                    treeView1.Nodes[item.IlID.ToString()].Nodes.Add(ilce.IlceAd);
+                }
+            }
+        }
+    }
+}
